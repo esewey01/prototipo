717090075ea2a84ba30a64ab4bfb1f52e768e0f4
David Ugarte Velásquez <83848466+esewey01@users.noreply.github.com>
2025-05-04 14:38:01 -0600

    HEAD NEW NEW
    aaaaaaaaaaaaaaaaaaa

--- Views/Head.php
+++ Views/Head.php
@@ -5,54 +5,54 @@
 
     <title>UPIICSA FOOD - Sistema de Compra y Venta</title>
 
-    <link href="public/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../public/css/bootstrap.min.css" rel="stylesheet">
     <!-- Bootstrap CSS -->
-    <link href="public/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../public/css/bootstrap.min.css" rel="stylesheet">
     <!-- bootstrap theme -->
-    <link href="public/css/bootstrap-theme.css" rel="stylesheet">
+    <link href="../public/css/bootstrap-theme.css" rel="stylesheet">
     <!--external css-->
     <!-- font icon -->
-    <link href="public/css/elegant-icons-style.css" rel="stylesheet" />
-    <link href="public/css/font-awesome.min.css" rel="stylesheet" />
+    <link href="../public/css/elegant-icons-style.css" rel="stylesheet" />
+    <link href="../public/css/font-awesome.min.css" rel="stylesheet" />
     <!-- full calendar css-->
-    <link href="public/css/assets/fullcalendar/fullcalendar/bootstrap-fullcalendar.css" rel="stylesheet" />
-    <link href="public/css/assets/fullcalendar/fullcalendar/fullcalendar.css" rel="stylesheet" />
+    <link href="../public/css/assets/fullcalendar/fullcalendar/bootstrap-fullcalendar.css" rel="stylesheet" />
+    <link href="../public/css/assets/fullcalendar/fullcalendar/fullcalendar.css" rel="stylesheet" />
     <!-- easy pie chart-->
     <!-- <link href="assets/jquery-easy-pie-chart/jquery.easy-pie-chart.css" rel="stylesheet" type="text/css" media="screen"/>-->
     <!-- owl carousel -->
-    <link rel="stylesheet" href="public/css/owl.carousel.css" type="text/css">
-    <link href="public/css/jquery-jvectormap-1.2.2.css" rel="stylesheet">
+    <link rel="stylesheet" href="../public/css/owl.carousel.css" type="text/css">
+    <link href="../public/css/jquery-jvectormap-1.2.2.css" rel="stylesheet">
     <!-- Custom styles -->
-    <link rel="stylesheet" href="public/css/fullcalendar.css">
-    <link href="public/css/widgets.css" rel="stylesheet">
-    <link href="public/css/style.css" rel="stylesheet">
-    <link href="public/css/style_pru.css" rel="stylesheet">
-    <link href="public/css/style-responsive.css" rel="stylesheet" />
-    <link href="public/css/xcharts.min.css" rel=" stylesheet">
-    <link href="public/css/jquery-ui-1.10.4.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../public/css/fullcalendar.css">
+    <link href="../public/css/widgets.css" rel="stylesheet">
+    <link href="../public/css/style.css" rel="stylesheet">
+    <link href="../public/css/style_pru.css" rel="stylesheet">
+    <link href="../public/css/style-responsive.css" rel="stylesheet" />
+    <link href="../public/css/xcharts.min.css" rel=" stylesheet">
+    <link href="../public/css/jquery-ui-1.10.4.min.css" rel="stylesheet">
 
-    <link href="public/css/anchomodal.css" rel="stylesheet">
+    <link href="../public/css/anchomodal.css" rel="stylesheet">
    
     <!-- Subir Imagen -->
-    <link href="public/css/fileinput.css" media="all" rel="stylesheet" type="text/css" />
+    <link href="../public/css/fileinput.css" media="all" rel="stylesheet" type="text/css" />
     <!-- Calendario -->
-    <link href="public/css/calendario.css" rel="stylesheet">
+    <link href="../public/css/calendario.css" rel="stylesheet">
     <!--    <link href="css/jquery-ui-timepicker-addon/jquery-ui-timepicker-addon.min.css" rel="stylesheet">-->
-    <link href="public/js/jquery-ui/jquery-ui.min.css" rel="stylesheet">
+    <link href="../public/js/jquery-ui/jquery-ui.min.css" rel="stylesheet">
     <!-- Ventana modal -->
-    <link rel="stylesheet" href="public/css/basicModal.min.css">
+    <link rel="stylesheet" href="../public/css/basicModal.min.css">
 
     <!-- Ventana modal style
     <link rel="stylesheet" type="text/css" href="css/default.css" />        -->
-    <link rel="stylesheet" type="text/css" href="public/css/component.css" />
+    <link rel="stylesheet" type="text/css" href="../public/css/component.css" />
 
-    <link rel="stylesheet" href="public/css/ngDialog.css">
-    <link rel="stylesheet" href="public/css/ngDialog-theme-default.css">
-    <link rel="stylesheet" href="public/css/ngDialog-theme-plain.css">
-    <link rel="stylesheet" href="public/css/ngDialog-custom-width.css">
+    <link rel="stylesheet" href="../public/css/ngDialog.css">
+    <link rel="stylesheet" href="../public/css/ngDialog-theme-default.css">
+    <link rel="stylesheet" href="../public/css/ngDialog-theme-plain.css">
+    <link rel="stylesheet" href="../public/css/ngDialog-custom-width.css">
 
 
-    <link href='public/css/adaptive-modal.css' rel='stylesheet' type='text/css'>
+    <link href='../public/css/adaptive-modal.css' rel='stylesheet' type='text/css'>
 
     <!-- letra style -->
     <!-- <link rel="stylesheet" href="css/letra.css">-->
