3515e61e180622e6adf50c44619d46f68a7a9db9
David Ugarte Velásquez <83848466+esewey01@users.noreply.github.com>
2025-05-04 14:29:29 -0600

    HEAD NEW

--- Views/Head.php
+++ Views/Head.php
@@ -5,58 +5,54 @@
 
     <title>UPIICSA FOOD - Sistema de Compra y Venta</title>
 
+    <link href="public/css/bootstrap.min.css" rel="stylesheet">
     <!-- Bootstrap CSS -->
-    <link href="<?= URL_VIEWS ?>css/bootstrap.min.css" rel="stylesheet">
+    <link href="public/css/bootstrap.min.css" rel="stylesheet">
     <!-- bootstrap theme -->
-    <link href="<?= URL_VIEWS ?>css/bootstrap-theme.css" rel="stylesheet">
+    <link href="public/css/bootstrap-theme.css" rel="stylesheet">
     <!--external css-->
     <!-- font icon -->
-    <link href="<?= URL_VIEWS ?>css/elegant-icons-style.css" rel="stylesheet" />
-    <link href="<?= URL_VIEWS ?>css/font-awesome.min.css" rel="stylesheet" />
+    <link href="public/css/elegant-icons-style.css" rel="stylesheet" />
+    <link href="public/css/font-awesome.min.css" rel="stylesheet" />
     <!-- full calendar css-->
-    <link href="<?= URL_VIEWS ?>css/assets/fullcalendar/fullcalendar/bootstrap-fullcalendar.css" rel="stylesheet" />
-    <link href="<?= URL_VIEWS ?>css/assets/fullcalendar/fullcalendar/fullcalendar.css" rel="stylesheet" />
+    <link href="public/css/assets/fullcalendar/fullcalendar/bootstrap-fullcalendar.css" rel="stylesheet" />
+    <link href="public/css/assets/fullcalendar/fullcalendar/fullcalendar.css" rel="stylesheet" />
     <!-- easy pie chart-->
     <!-- <link href="assets/jquery-easy-pie-chart/jquery.easy-pie-chart.css" rel="stylesheet" type="text/css" media="screen"/>-->
     <!-- owl carousel -->
-    <link rel="stylesheet" href="<?= URL_VIEWS ?>css/owl.carousel.css" type="text/css">
-    <link href="<?= URL_VIEWS ?>css/jquery-jvectormap-1.2.2.css" rel="stylesheet">
+    <link rel="stylesheet" href="public/css/owl.carousel.css" type="text/css">
+    <link href="public/css/jquery-jvectormap-1.2.2.css" rel="stylesheet">
     <!-- Custom styles -->
-    <link rel="stylesheet" href="<?= URL_VIEWS ?>css/fullcalendar.css">
-    <link href="<?= URL_VIEWS ?>css/widgets.css" rel="stylesheet">
-    <link href="<?= URL_VIEWS ?>css/style.css" rel="stylesheet">
-    <link href="<?= URL_VIEWS ?>css/style_pru.css" rel="stylesheet">
-    <link href="<?= URL_VIEWS ?>css/style-responsive.css" rel="stylesheet" />
-    <link href="<?= URL_VIEWS ?>css/xcharts.min.css" rel=" stylesheet">
-    <link href="<?= URL_VIEWS ?>css/jquery-ui-1.10.4.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="public/css/fullcalendar.css">
+    <link href="public/css/widgets.css" rel="stylesheet">
+    <link href="public/css/style.css" rel="stylesheet">
+    <link href="public/css/style_pru.css" rel="stylesheet">
+    <link href="public/css/style-responsive.css" rel="stylesheet" />
+    <link href="public/css/xcharts.min.css" rel=" stylesheet">
+    <link href="public/css/jquery-ui-1.10.4.min.css" rel="stylesheet">
 
-    <link href="<?= URL_VIEWS ?>css/anchomodal.css" rel="stylesheet">
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 -->
-    <!--[if lt IE 9]>
-    <script src="<?= URL_VIEWS ?>js/html5shiv.js"></script>
-    <script src="<?= URL_VIEWS ?>js/respond.min.js"></script>
-    <script src="<?= URL_VIEWS ?>js/lte-ie7.js"></script>
-    <![endif]-->
+    <link href="public/css/anchomodal.css" rel="stylesheet">
+   
     <!-- Subir Imagen -->
-    <link href="<?= URL_VIEWS ?>css/fileinput.css" media="all" rel="stylesheet" type="text/css" />
+    <link href="public/css/fileinput.css" media="all" rel="stylesheet" type="text/css" />
     <!-- Calendario -->
-    <link href="<?= URL_VIEWS ?>css/calendario.css" rel="stylesheet">
+    <link href="public/css/calendario.css" rel="stylesheet">
     <!--    <link href="css/jquery-ui-timepicker-addon/jquery-ui-timepicker-addon.min.css" rel="stylesheet">-->
-    <link href="<?= URL_VIEWS ?>js/jquery-ui/jquery-ui.min.css" rel="stylesheet">
+    <link href="public/js/jquery-ui/jquery-ui.min.css" rel="stylesheet">
     <!-- Ventana modal -->
-    <link rel="stylesheet" href="<?= URL_VIEWS ?>css/basicModal.min.css">
+    <link rel="stylesheet" href="public/css/basicModal.min.css">
 
     <!-- Ventana modal style
     <link rel="stylesheet" type="text/css" href="css/default.css" />        -->
-    <link rel="stylesheet" type="text/css" href="<?= URL_VIEWS ?>css/component.css" />
+    <link rel="stylesheet" type="text/css" href="public/css/component.css" />
 
-    <link rel="stylesheet" href="<?= URL_VIEWS ?>css/ngDialog.css">
-    <link rel="stylesheet" href="<?= URL_VIEWS ?>css/ngDialog-theme-default.css">
-    <link rel="stylesheet" href="<?= URL_VIEWS ?>css/ngDialog-theme-plain.css">
-    <link rel="stylesheet" href="<?= URL_VIEWS ?>css/ngDialog-custom-width.css">
+    <link rel="stylesheet" href="public/css/ngDialog.css">
+    <link rel="stylesheet" href="public/css/ngDialog-theme-default.css">
+    <link rel="stylesheet" href="public/css/ngDialog-theme-plain.css">
+    <link rel="stylesheet" href="public/css/ngDialog-custom-width.css">
 
 
-    <link href='<?= URL_VIEWS ?>css/adaptive-modal.css' rel='stylesheet' type='text/css'>
+    <link href='public/css/adaptive-modal.css' rel='stylesheet' type='text/css'>
 
     <!-- letra style -->
     <!-- <link rel="stylesheet" href="css/letra.css">-->
