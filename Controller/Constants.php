0cbc266b3879de077472092ae4b1a215a0613701
David Ugarte Velásquez <83848466+esewey01@users.noreply.github.com>
2025-05-04 13:31:41 -0600

    zsdf
    asdg

--- Controller/Constants.php
+++ Controller/Constants.php
@@ -1,13 +1,17 @@
 <?php
 if (!defined('URL_VIEWS')) {
-    $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? "https" : "http";
+    $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? 'https' : 'http';
     $host = $_SERVER['HTTP_HOST'];
-    $path = str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
-    define('URL_VIEWS', "{$protocol}://{$host}{$path}Views/");
-}
 
+    // Ruta base del proyecto (sube dos niveles desde el archivo en ejecución)
+    $base_path = dirname(dirname($_SERVER['SCRIPT_NAME'])); // /Prototipo
+    $base_path = rtrim($base_path, '/'); // Elimina cualquier '/' al final
+
+    define('URL_VIEWS', "{$protocol}://{$host}{$base_path}/Views/");
+}
 
 if (!defined('ADDRESS')) {
-    define('ADDRESS', $_SERVER['DOCUMENT_ROOT'] . '/Prototipo/Views/fotoproducto/');
+    // Ruta del sistema de archivos al directorio de fotos
+    $root_path = dirname(dirname(__DIR__)); // Sube dos niveles desde el archivo actual
+    define('ADDRESS', $root_path . '/Views/fotoproducto/');
 }
-
