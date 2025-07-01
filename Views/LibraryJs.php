f415bc7e7d143ce784976bd5e700c8dd22996099
David Ugarte Velásquez <83848466+esewey01@users.noreply.github.com>
2025-07-01 00:23:11 -0600

    tons

--- Views/LibraryJs.php
+++ Views/LibraryJs.php
@@ -1,47 +1,46 @@
+<!-- jQuery (solo UNA versión) -->
+<script src="<?=URL_PUBLIC?>js/jquery-1.12.4.min.js"></script>
 
-<!-- javascripts -->
-<script src="<?=URL_PUBLIC?>js/jquery.js"></script>
-<script src="<?=URL_PUBLIC?>js/jquery-ui-1.10.4.min.js"></script>
-<script src="<?=URL_PUBLIC?>js/jquery-1.8.3.min.js"></script>
-<script type="text/javascript" src="<?=URL_PUBLIC?>js/jquery-ui-1.9.2.custom.min.js"></script>
-<!-- bootstrap -->
+<!-- jQuery Migrate para compatibilidad -->
+<script src="https://code.jquery.com/jquery-migrate-1.4.1.min.js"></script>
+
+<!-- jQuery UI -->
+<script src="<?=URL_PUBLIC?>js/jquery-ui-1.12.1.min.js"></script>
+
+<!-- Bootstrap -->
 <script src="<?=URL_PUBLIC?>js/bootstrap.min.js"></script>
-<!-- nice scroll -->
+
+<!-- Otros plugins (ordenados según dependencias) -->
 <script src="<?=URL_PUBLIC?>js/jquery.scrollTo.min.js"></script>
-<script src="<?=URL_PUBLIC?>js/jquery.nicescroll.js" type="text/javascript"></script>
-<!-- charts scripts -->
-
-<script src="<?=URL_PUBLIC?>js/jquery.sparkline.js" type="text/javascript"></script>
+<script src="<?=URL_PUBLIC?>js/jquery.nicescroll.js"></script>
+<script src="<?=URL_PUBLIC?>js/jquery.sparkline.js"></script>
 <script src="<?=URL_PUBLIC?>js/owl.carousel.js"></script>
-
-<!--script for this page only-->
-<!-----------------------------------<script src="js/calendar-custom.js"></script>----->
 <script src="<?=URL_PUBLIC?>js/jquery.rateit.min.js"></script>
-<!-- custom select -->
 <script src="<?=URL_PUBLIC?>js/jquery.customSelect.min.js"></script>
-
-
-<!--custome script for all page-->
-<script src="<?=URL_PUBLIC?>js/scripts.js"></script>
-<!-- custom script for this page-->
-<script src="<?=URL_PUBLIC?>js/sparkline-chart.js"></script>
 <script src="<?=URL_PUBLIC?>js/jquery-jvectormap-1.2.2.min.js"></script>
 <script src="<?=URL_PUBLIC?>js/jquery-jvectormap-world-mill-en.js"></script>
-<script src="<?=URL_PUBLIC?>js/xcharts.min.js"></script>
 <script src="<?=URL_PUBLIC?>js/jquery.autosize.min.js"></script>
 <script src="<?=URL_PUBLIC?>js/jquery.placeholder.min.js"></script>
-<script src="<?=URL_PUBLIC?>js/gdp-data.js"></script>
-<script src="<?=URL_PUBLIC?>js/morris.min.js"></script>
-<script src="<?=URL_PUBLIC?>js/sparklines.js"></script>
-<script src="<?=URL_PUBLIC?>js/charts.js"></script>
 <script src="<?=URL_PUBLIC?>js/jquery.slimscroll.min.js"></script>
-<script src="<?=URL_PUBLIC?>js/zabuto_calendar.js"></script>
+
+<!-- DataTables -->
+<script src="<?=URL_PUBLIC?>js/jquery.dataTables.min.js"></script>
+<script src="<?=URL_PUBLIC?>js/dataTables.bootstrap.min.js"></script>
+
+<!-- Toastr -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+
+<!-- Scripts personalizados -->
+<script src="<?=URL_PUBLIC?>js/scripts.js"></script>
+<script src="<?=URL_PUBLIC?>js/sparkline-chart.js"></script>
+<script src="<?=URL_PUBLIC?>js/charts.js"></script>
 <script src="<?=URL_PUBLIC?>js/ajax.js"></script>
-<script language="JavaScript" type="text/javascript"src="<?=URL_PUBLIC?>js/ajaxPos.js" ></script>
+<script src="<?=URL_PUBLIC?>js/ajaxPos.js"></script>
 
-
-
-<script type="application/javascript">
+<!-- Zabuto Calendar -->
+<script src="<?=URL_PUBLIC?>js/zabuto_calendar.js"></script>
+<script>
     $(document).ready(function () {
         $("#my-calendar").zabuto_calendar({
             language: "es",
@@ -54,18 +53,7 @@
     });
 </script>
 
-<script src="https://code.jquery.com/jquery-3.6.0.min.js "></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap @4.6.2/dist/js/bootstrap.bundle.min.js"></script>
-
-<!-- DataTables CSS -->
-<link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap.min.css" rel="stylesheet">
-<!-- DataTables JavaScript -->
-<script src="<?=URL_PUBLIC?>js/jquery.dataTables.min.js"></script>
-<script src="<?=URL_PUBLIC?>js/dataTables.bootstrap.min.js"></script>
-<!-- En tu LibraryJs.php o donde cargues los scripts -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
-
+<!-- DataTables Init -->
 <script>
     $(document).ready(function () {
         $('#dataTables-example').DataTable({
@@ -74,12 +62,10 @@
     });
 </script>
 
-
-<!--<script src="--><?php //echo $urlViews; ?><!--/js/print/jquery-1.4.4.min.js" type="text/javascript"></script>-->
+<!-- PrintPage (reemplaza por versión actualizada si es posible) -->
 <script src="<?=URL_PUBLIC?>/js/print/jquery.printPage.js" type="text/javascript"></script>
 <script>
     $(document).ready(function () {
         $(".btnPrint").printPage();
     });
 </script>
-
